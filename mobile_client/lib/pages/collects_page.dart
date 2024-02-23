f6b7d50f1cfb0ce875df1cbfd3b4fc0443b57f11
MateusSartorio <mateussartorio39@gmail.com>
2024-02-22 23:59:53 -0300

    formulário rústico para salvamento de peso

--- mobile_client/lib/pages/collects_page.dart
+++ mobile_client/lib/pages/collects_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_client/models/resident.dart';
+import 'package:mobile_client/store/global_state.dart';
+import 'package:provider/provider.dart';
 
-class CollectsPage extends StatelessWidget {
+class CollectsPage extends StatefulWidget {
   const CollectsPage({Key? key}) : super(key: key);
 
   @override
+  State<CollectsPage> createState() => _CollectsPageState();
+}
+
+class _CollectsPageState extends State<CollectsPage> {
+  Resident? currentlySelected;
+
+  final TextEditingController _weightController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Consumer<GlobalState>(
+        builder: (context, value, child) => Center(
+              child: FractionallySizedBox(
+                widthFactor: 0.8,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    DropdownButtonFormField<Resident>(
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(12),
+                              borderSide: const BorderSide(
+                                  width: 3, color: Colors.blue))),
+                      onChanged: (item) => currentlySelected = item,
+                      value: currentlySelected,
+                      items: value.residents
+                          .map((e) =>
+                              DropdownMenuItem(value: e, child: Text(e.name)))
+                          .toList(),
+                    ),
+                    TextField(
+                      controller: _weightController,
+                      decoration: const InputDecoration(
+                          hintText: "Peso [kg]", border: OutlineInputBorder()),
+                    ),
+                  ],
+                ),
+              ),
+            ));
   }
 }
