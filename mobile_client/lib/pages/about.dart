f33a5ca1b3a24044e47b1b8d5c3025347dd84129
MateusSartorio <mateussartorio39@gmail.com>
2024-03-17 22:46:23 -0300

    simple about page
    instagram link not working yet

--- mobile_client/lib/pages/about.dart
+++ mobile_client/lib/pages/about.dart
@@ -1,7 +1,18 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class About extends StatelessWidget {
   const About({Key? key}) : super(key: key);
+
+  void _launchURL(Uri url) async {
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,9 +33,65 @@ class About extends StatelessWidget {
                 icon: const Icon(Icons.arrow_back_outlined),
               ),
             )),
-        body: const Center(
-          child: Text("Sobre a Roka...",
-              style: TextStyle(fontWeight: FontWeight.w900, fontSize: 25)),
+        body: Center(
+          child: FractionallySizedBox(
+            widthFactor: 0.8,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                FractionallySizedBox(
+                  widthFactor: 0.7,
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                  ),
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  "Roka é um projeto social focado em resolver o problema do plástico de forma inovadora. Através de educação ambiental e coleta personalizada, incentivamos a comunidade a se engajar na destinação adequada do plástico. Com nossa abordagem única, estamos criando um futuro mais sustentável para todos. Junte-se a nós nessa jornada! 🌍💚",
+                  style: TextStyle(fontSize: 16),
+                  textAlign: TextAlign.justify,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const Text(
+                  "Conheça mais sobre o projeto",
+                  style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Icon(FontAwesomeIcons.instagram, size: 30),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    RichText(
+                      text: TextSpan(
+                        text: "@projetoroka",
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            String backendRoute =
+                                "https://www.instagram.com/projetoroka/";
+                            Uri uri = Uri.parse(backendRoute);
+                            _launchURL(uri);
+                          },
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
         ));
   }
 }
